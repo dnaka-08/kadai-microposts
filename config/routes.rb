557482e4d28042f44e99a0cf8046a2c09eb8dd25
daisuke.naka <ec2-user@ip-172-31-85-21.ec2.internal>
2020-01-20 18:02:31 +0000

    favorite function ver1.0

--- config/routes.rb
+++ config/routes.rb
@@ -5,15 +5,21 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  
+
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :new, :show, :create] do
     member do
      get :followings
      get :followers
+     get 'likes', to: 'favorites'
     end
   end
   
-  resources :microposts, only: [:create, :destroy]
+  resources :microposts, only: [:create, :destroy] do
+    member do
+      get :favoriters
+    end
+  end
   resources :relationships, only: [:create, :destroy]
+  resources :favorites, only: [:create, :destroy]
 end
