fe22128892e3b375264bfd938625255a1a524853
daisuke.naka <ec2-user@ip-172-31-85-21.ec2.internal>
2020-01-27 16:16:51 +0000

    favorite function ver1.4

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,12 +4,12 @@ class FavoritesController < ApplicationController
   def create
     current_user.favorite(params[:micropost_id])
     flash[:success] = 'お気に入りを登録しました。'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     current_user.unfavorite(params[:micropost_id])
     flash[:success] = 'お気に入りを解除しました。'
-    redirect_to root_url
+    redirect_back(fallback_location: root_path)
   end
 end
